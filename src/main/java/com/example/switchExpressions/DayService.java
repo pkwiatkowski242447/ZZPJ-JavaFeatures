4fb55e4a6e8648b640cc65b5fe1da36e1e0904e2
242493 Piestrzeniewicz Michał <242493@edu.p.lodz.pl>
2024-03-29 23:19:25 +0100

    stringMets, switchEx, var

--- src/main/java/com/example/switchExpressions/DayService.java
+++ src/main/java/com/example/switchExpressions/DayService.java
@@ -3,9 +3,11 @@ package com.example.switchExpressions;
 public class DayService {
 
     int getDayNumberOfLettersForWorkday(Day day) {
-        // TODO: implement here
         // return number of letters for working days or throw exception for the other
 
-        return 0;
+        return switch (day) {
+            case  MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY -> day.name().length();
+            default -> throw new IllegalStateException();
+        };
     }
 }
